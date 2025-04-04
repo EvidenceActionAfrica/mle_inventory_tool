d47654ae7b1297f4ff20cc0a1333a4ddb837c04a
mskogi <rita.kogi@evidenceaction.org>
2025-04-04 14:28:26 +0300

    typo

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -33,7 +33,7 @@ $role = $_SESSION['role'] ?? null;
 
       <?php if($role === 'admin' ): ?>
       <div class="card" style="background-color:#e600a0;" 
-           onClick="to_url('<?php echo URL . 'inventorysssignment'; ?>')">
+           onClick="to_url('<?php echo URL . 'inventoryassignment'; ?>')">
         <b style="color: #fff; margin-left:3rem; font-family: ArchivoBlack; font-size:20px;">MLE INVENTORY TOOL</b>
       </div>
       <?php endif; ?>
